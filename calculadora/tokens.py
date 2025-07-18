416d25afefd5d9da4d9cd1dc6590b73e8c408e04
Elias_Oliveira <eliasoliveiradapaz@hotmail.com>
2025-07-17 21:14:27 -0300

    add: class token

--- calculadora/tokens.py
+++ calculadora/tokens.py
@@ -11,3 +11,15 @@ class TokenType(Enum):
     RPAREN  = 'RPAREN'  
     EOF     = 'EOF'
 
+class Token:
+    """
+    Representa um token, a menor unidade de significado.
+    Ex: Token(TokenType.INTEGER, 10)
+    """
+    def __init__(self, type: TokenType, value: any = None):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        """Representação em string para facilitar a depuração."""
+        return f'Token({self.type.name}, {repr(self.value)})'
