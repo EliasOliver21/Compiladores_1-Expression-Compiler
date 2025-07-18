f5166e76cef74d1548d7160a58ab3b55e91254b0
claudiohsc <claudio885henrique@gmail.com>
2025-07-18 18:27:10 -0300

    add interpreter final

--- calculadora/interpreter.py
+++ calculadora/interpreter.py
@@ -8,4 +8,26 @@ class Interpreter:
         visitor = getattr(self, method_name, self.generic_visit)
         return visitor(node)
 
-    
+    def generic_visit(self, node: ASTNode):
+        raise NotImplementedError(f"Nenhum método visit{type(node).name} definido")
+
+    def _visit_NumberNode(self, node: NumberNode):
+        """Visita um nó de número."""
+        return node.value
+
+    def _visit_BinOpNode(self, node: BinOpNode):
+        """Visita um nó de operação binária"""
+        left_val = self.visit(node.left)
+        right_val = self.visit(node.right)
+
+        if node.op.type == TokenType.PLUS:
+            return left_val + right_val
+        if node.op.type == TokenType.MINUS:
+            return left_val - right_val
+        if node.op.type == TokenType.MUL:
+            return left_val * right_val
+        if node.op.type == TokenType.DIV:
+            # Evitar divisão por zero
+            if right_val == 0:
+                raise ZeroDivisionError("Divisão por zero")
+            return left_val / right_val
